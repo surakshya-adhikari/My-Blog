0814ed51913c7f1687206036e860301619b4d5c5
surakshya-adhikar <surakshyaadhikari09@gmail.com>
2025-03-01 21:29:21 +0545

    Some Small Changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@
 import './App.css'
 
 function App() {
-  
+      console.log(import.meta.env.VITE_APPWRITE_URL)
 
   return (
     <>
-      <h1> Welcome to my blog</h1>
+      <h1> Welcomezz</h1>
     </>
   )
 }
