cdbb9deff2355d473d5eb7cd307e3f894414c1c8
surakshya-adhikar <surakshyaadhikari09@gmail.com>
2025-03-06 19:40:32 +0545

    Implement user authentication check on app load with Appwrite and Redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,43 @@
-
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { Outlet } from 'react-router-dom'
 import './App.css'
+import authService from "./appwrite/auth"
+import {  Footer, Header } from './components'
+import { login, logout } from "./store/authSlice"
+
 
 function App() {
-      console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1 className='text-4xl bg-amber-500 text-emerald-600'> Welcomezz</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+ 
+
+  
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        TODO:  <Outlet />
+        </main>
+        <Footer/>
+        
+        
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
