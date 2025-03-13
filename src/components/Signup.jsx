7093c2a596d03dcc9de5397ed8262b980bde65ba
surakshya-adhikar <surakshyaadhikari09@gmail.com>
2025-03-13 15:00:47 +0545

    Minor Changes

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React,{useState} from 'react'
 import authService from '../appwrite/auth'
 import {Link ,useNavigate} from 'react-router-dom'
 import {login} from '../store/authSlice'
 import {Button, Input, Logo} from './index.js'
 import {useDispatch} from 'react-redux'
 import {useForm} from 'react-hook-form'
+import { useState } from 'react'
 
 function Signup() {
     const navigate = useNavigate()
@@ -17,7 +18,7 @@ function Signup() {
         try {
             const userData = await authService.createAccount(data)
             if (userData) {
-                const userData = await authService.getCurrentUser()
+                 userData = await authService.getCurrentUser()
                 if(userData) dispatch(login(userData));
                 navigate("/")
             }
