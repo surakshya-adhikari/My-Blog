0d9b4c2d7a30fae6e154ba34005ebf062b22c66b
surakshya-adhikar <surakshyaadhikari09@gmail.com>
2025-03-14 20:13:41 +0545

    Solved Import Problem

--- src/main.jsx
+++ src/main.jsx
@@ -1,41 +1,84 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-// import {Provider} from 'react-redux'
-// import store from './store/store.js'
-
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <Provider store={store}>
-//     <RouterProvider router={router}/>
-//     </Provider>
-    
-//   </StrictMode>,
-// )
-
-
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-import { Provider } from 'react-redux';
-import store from './store/store.js';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/pages/Home.jsx";
+import { AuthLayout, Login } from "./components/index.js";
+import AddPost from "./components/pages/AddPost";
+import Post from "./components/pages/Post.jsx";
+import Signup from "./components/pages/Signup";
+import EditPost from "./components/pages/EditPost";
+import AllPost from "./components/pages/AllPost";
 
 // Define the router
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App />,
-    },
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: (
+          <AuthLayout authentication={false}>
+            <Login />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/signup",
+        element: (
+          <AuthLayout authentication={false}>
+            <Signup />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/all-post",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <AllPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/add-post",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <AddPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/edit-post/:slug",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <EditPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/post/:slug",
+        element: <Post />,
+      },
+    ],
+  },
 ]);
 
-createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <Provider store={store}>
-            <RouterProvider router={router} />
-        </Provider>
-    </StrictMode>,
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
 );
